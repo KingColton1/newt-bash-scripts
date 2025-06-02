0f1405b01bc09911a2a12a84030c746736277a59
Colton B <main@coltvel.com>
2025-06-02 02:33:01 +0000

    Create install-newt.sh

---
+++ install-newt.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+wget -O newt "https://github.com/fosrl/newt/releases/download/1.1.3/newt_linux_amd64" && chmod +x ./newt && sudo mv ./newt /usr/local/bin
+
+read -p "Enter Newt ID: " NEWT_ID
+read -sp "Enter Newt Secret: " NEWT_SECRET
+echo
+read -p "Enter Newt Endpoint (your pangolin's domain): " NEWT_ENDPOINT
+echo
+
+sudo tee /etc/systemd/system/newt.service > /dev/null <<EOF
+[Unit]
+Description=Newt VPN Client
+After=network.target
+
+[Service]
+ExecStart=/usr/local/bin/newt --id $NEWT_ID --secret $NEWT_SECRET --endpoint $NEWT_ENDPOINT
+Restart=always
+User=root
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo chmod 600 /etc/systemd/system/newt.service
+sudo systemctl daemon-reload
+systemctl enable newt.service
+systemctl start newt.service
+systemctl status newt.service
